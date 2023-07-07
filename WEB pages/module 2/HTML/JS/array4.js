3b98ffdabcaadb97abd5a32072e68e395dc4e4ea
likith <likithroman12@gmail.com>
2023-07-07 08:52:19 +0530

    doe

---
+++ WEB pages/module 2/HTML/JS/array4.js
@@ -0,0 +1,18 @@
+//map and filter in this
+let nos=[1,2,3,4,5,6,7,8];
+/*nos.map(
+    funtion (n)    //n means each element in the array
+    {
+Console.log(n);
+
+    }
+)*/
+var m=0;
+nos.map( funtion(n)
+    {
+        if ( m < n ){
+            m=n;
+                   }        
+    }
+);
+console.log("the largest no is" ,m);
